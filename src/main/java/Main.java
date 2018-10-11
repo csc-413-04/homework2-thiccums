36bc0f34110f3f7e14f858b3fa9cd335f4548be7
brianxautumn <shred@brianautumn.com>
2018-10-10 19:54:59 -0700

    reorganized project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,14 @@
-package html;
+package main.java;
 import static spark.Spark.*;
-import static com.mongodb.client.model.Filters.*;
-import com.mongodb.*;
 
 public class Main {
 
     public static void main(String[] args) {
+      // staticFiles.externalLocation("public");
+      // http://sparkjava.com/documentation
       port(1234);
       // calling get will make your app start listening for the GET path with the /hello endpoint
       get("/hello", (req, res) -> "Hello World");
+
     }
 }
