01fa1723a21bc3ffe4fb0ee9d7c46675d6610461
brianxautumn <shred@brianautumn.com>
2018-10-11 21:02:30 -0700

    Class demo

--- public/script.js
+++ public/script.js
@@ -1,1 +1,71 @@
+// These are console log types
 console.log('hello world');
+// console.warn('this is a warning');
+// console.error('this is an error');
+
+let myVariable;
+const myOtherVariable = 4;
+myVariable = [8, 23, 'asd'];
+
+const myObject = {};
+myObject.a = 8;
+myObject.b = 9;
+
+console.log(myVariable);
+console.log(myObject);
+
+// functions 
+function myFunction() {
+    const oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", (response) => {
+        // this will not execute until the response is done
+        const data = JSON.parse(oReq.responseText);
+        const humidity = data.query.results.channel.atmosphere.humidity;
+        console.log(humidity);
+        document.getElementById('humidity').innerHTML = humidity
+        // selects elements by id from the document tree
+        //document.getElementById('output').innerHTML = oReq.responseText;
+    });
+    oReq.open("GET", "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
+    oReq.send();
+}
+
+function pushCoinNum() {
+    const oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", (response) => {
+        // this will not execute until the response is done
+        const data = JSON.parse(oReq.responseText);
+        console.log(data);
+        // selects elements by id from the document tree
+        document.getElementById('output').innerHTML = oReq.responseText;
+    });
+    oReq.open("GET", "/counterUpdate");
+    oReq.send();
+}
+
+function statusUpdate() {
+    const oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", (response) => {
+        // this will not execute until the response is done
+        const data = JSON.parse(oReq.responseText);
+        console.log(data);
+        // selects elements by id from the document tree
+        document.getElementById('output').innerHTML = oReq.responseText;
+    });
+    oReq.open("GET", "/status");
+    oReq.send();
+}
+
+myFunction();
+
+// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
+
+// arrow functions
+const myArrowFunction = () => {
+    setTimeout(() => {
+        statusUpdate();
+        myArrowFunction();
+    }, 200);
+};
+
+myArrowFunction();
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,20 @@ package main.java;
 import static spark.Spark.*;
 
 public class Main {
-
+    static int counter = 0;
     public static void main(String[] args) {
-      // staticFiles.externalLocation("public");
+
+      staticFiles.externalLocation("public");
       // http://sparkjava.com/documentation
       port(1234);
       // calling get will make your app start listening for the GET path with the /hello endpoint
-      get("/hello", (req, res) -> "Hello World");
+      get("/counterUpdate", (req, res) -> {
+        // Main.counter++;
+        return ++Main.counter;
+      });
 
+      get("/status", (req, res) -> {
+        return Main.counter;
+      });
     }
 }
