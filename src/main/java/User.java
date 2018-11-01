66de587535071de79d6ab2a7333fc0e287a721fa
loucid <syd.morano@gmail.com>
2018-11-01 10:24:17 -0700

    Some getter/setter stuff

---
+++ src/main/java/User.java
@@ -0,0 +1,41 @@
+package main.java;
+
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+
+@Entity
+public class User{
+    @Id
+    private ObjectId id;
+    private String username;
+    private String password;
+
+    // just some getters and setters
+    
+    public User(){
+    }
+    public User(String username, String password){
+        super();
+        this.username = username;
+        this.password = password;
+    }
+    public ObjectId getId() {
+		return id;
+	}
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+	public String getUser() {
+		return username;
+	}
+	public void setUser(String username) {
+		this.username = username;
+	}
+	public String getPass() {
+		return password;
+	}
+	public void setPass(String password) {
+		this.password = password;
+	}
+}
